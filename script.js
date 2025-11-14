bb171fcff32fdaa457183c71250838fa33f2a633
thái trần <113418108+thai12340pa@users.noreply.github.com>
2025-11-14 10:06:25 +0700

    Fix betAmount condition for value 100

--- script.js
+++ script.js
@@ -46,9 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
             betAmount = 50;
         } else if (betAmount === 50) {
             betAmount = 100;
-         } else if (betAmount === 100) {
+        } else if (betAmount === 100) {
             betAmount = 10000;
-        {
         } else {
             betAmount = 10;
         }
